45426e86fb3286c4dd40a6595ae32744b64b2085
YYX-computer <59608785+YYX-computer@users.noreply.github.com>
2020-06-10 18:57:08 +0800

    Update ratingBar.py

--- ratingBar.py
+++ ratingBar.py
@@ -5,18 +5,19 @@ def n_digit(n,string):
 	return string
 
 class ratingBar:
-	def __init__(self,length,starting = ''):
+	def __init__(self,length,starting = '',ending = ''):
 		self.length = length
 		self.one = 100 / length
 		self.n = len(str(self.length))
 		self.starting = starting
-		sys.stdout.write('%s%s%s/%s|%s|0%%'%(self.starting,'|' * int(bool(self.starting)),n_digit(self.n,'0'),length,' ' * length))
+		self.ending = ending
+		sys.stdout.write('%s%s%s/%s|%s|0%% %s'%(self.starting,'|' * int(bool(self.starting)),n_digit(self.n,'0'),length,' ' * length，self.ending))
 		sys.stdout.flush()
 		self.len = 0
 	def flush(self,length):
 		str1 = '#' * length + ' ' * (self.length - length)
 		sys.stdout.flush()
-		sys.stdout.write('\r%s%s%s/%s|%s|%s%%'%(self.starting,'|' * int(bool(self.starting)),n_digit(self.n,str(length)),self.length,str1,self.one * length))
+		sys.stdout.write('\r%s%s%s/%s|%s|%s%% %s'%(self.starting,'|' * int(bool(self.starting)),n_digit(self.n,str(length)),self.length,str1,self.one * length，self.ending))
 		if(length == self.length):
 			sys.stdout.write('\n')
 			sys.stdout.flush()
