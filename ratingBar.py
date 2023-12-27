123c42482ed65e3375a6e0e0f1a5c0b208f24fe0
YYX-computer <59608785+YYX-computer@users.noreply.github.com>
2023-12-27 12:56:21 +0800

    Update ratingBar.py

--- ratingBar.py
+++ ratingBar.py
@@ -3,7 +3,7 @@ def complete_string(n,string):
 	while(len(string) < n):
 		string += ' '
 	return string
-
+n_digit = lambda a,b:'0' * (a - len(b)) + b
 class ratingBar:
 	def __init__(self,length,starting = '',ending = ''):
 		self.length = length
@@ -11,20 +11,21 @@ class ratingBar:
 		self.n = len(str(self.length))
 		self.starting = starting
 		self.ending = ending
-		sys.stdout.write('%s%s%s/%s|%s|0%% %s'%(self.starting,'|' * int(bool(self.starting)),n_digit(self.n,'0'),length,' ' * length，self.ending))
+		sys.stdout.write('%s%s%s/%s|%s|0%% %s'%(self.starting,'|' * int(bool(self.starting)),n_digit(self.n,'0'),length,' ' * length,self.ending))
 		sys.stdout.flush()
 		self.len = 0
 	def flush(self,length):
 		if(length <= self.length):
 			str1 = '#' * length + ' ' * (self.length - length)
 			sys.stdout.flush()
-			sys.stdout.write('\r%s%s%s/%s|%s|%s%% %s'%(self.starting,'|' * int(bool(self.starting)),n_digit(self.n,str(length)),self.length,str1,self.one * length，self.ending))
+			sys.stdout.write('\r%s%s%s/%s|%s|%s%% %s'%(self.starting,'|' * int(bool(self.starting)),n_digit(self.n,str(length)),self.length,str1,self.one * length,self.ending))
 			if(length == self.length):
 				sys.stdout.write('\n')
 				sys.stdout.flush()
 			self.len = length
 		else:
 			raise ValueError('index out of range')
-	def count(self):
-		self.len += 1
+	def count(self,step = 1):
+		self.len = max(min(self.len + step,self.length),0)
 		self.flush(self.len)
+		return self.len == self.length
